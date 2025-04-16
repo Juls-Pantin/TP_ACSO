840953d0bc5de511aa2b37bbf529116e26a770e2
Julia Pantin <69682517+Juan-2001@users.noreply.github.com>
2025-04-16 01:06:19 -0300

    1

--- TP2-x86_64/ej1/ej1.h
+++ TP2-x86_64/ej1/ej1.h
@@ -5,8 +5,8 @@
 #include <assert.h>
 #include <stdbool.h>
 
-//#define USE_ASM_IMPL 1
-#define USE_ASM_IMPL 0
+#define USE_ASM_IMPL 1
+//#define USE_ASM_IMPL 0
 
 /** Lista **/
 typedef struct string_proc_list_t {
