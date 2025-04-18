b9410705a01e3f4123262a87d4b14010282e2917
Julia Pantin <69682517+Juan-2001@users.noreply.github.com>
2025-04-17 21:09:58 -0300

    Update runTester.sh

--- TP2-x86_64/ej1/runTester.sh
+++ TP2-x86_64/ej1/runTester.sh
@@ -18,7 +18,7 @@ ERRORDIFF=0
 
 ./tester
 
-$DIFFER salida.caso.propio.ej1.txt salida.catedra.ej1.txt > /Users/juliapantin/Documents/GitHub/TP_ACSO/TP2-x86_64/ej1
+$DIFFER salida.caso.propio.ej1.txt salida.catedra.ej1.txt > diff.ej1.txt
 if [ $? -ne 0 ]; then
   echo "  **Discrepancia en el ejercicio 1"
   ERRORDIFF=1
