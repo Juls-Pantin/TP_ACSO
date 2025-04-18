c36993fccb9c36d34599724148b6ade5736a1947
Julia Pantin <69682517+Juan-2001@users.noreply.github.com>
2025-04-18 11:11:44 -0300

    Update ej1.asm

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -15,121 +15,152 @@ string_proc_list_create_asm:
     mov rdi, 16
     call malloc
     test rax, rax
-    je .fail_list
+    jz .fail
     mov qword [rax], NULL
-    mov qword [rax + 8], NULL
+    mov qword [rax+8], NULL
     ret
-.fail_list:
+.fail:
     xor rax, rax
     ret
 
 string_proc_node_create_asm:
     test rsi, rsi
-    je .fail_node
-    mov r8b, dil
-    mov r9, rsi
+    jz .fail_node
+
+    movzx ecx, dil       ; type -> ecx
+    mov r8, rsi          ; hash -> r8
 
     mov rdi, 32
     call malloc
     test rax, rax
-    je .fail_node
+    jz .fail_node
 
     mov qword [rax], NULL
-    mov qword [rax + 8], NULL
-    mov byte [rax + 16], r8b
-    mov qword [rax + 24], r9
+    mov qword [rax+8], NULL
+    mov byte [rax+16], cl
+    mov qword [rax+24], r8
     ret
+
 .fail_node:
     xor rax, rax
     ret
 
 string_proc_list_add_node_asm:
     test rdi, rdi
-    je .end_add
+    jz .end_add
 
-    mov r8, rdi        ; list
-    mov r9, rsi        ; type
-    mov r10, rdx       ; hash
+    mov r8, rdi       ; list
+    mov r9b, sil      ; type
+    mov r10, rdx      ; hash
 
     movzx edi, r9b
     mov rsi, r10
     call string_proc_node_create_asm
     test rax, rax
-    je .end_add
+    jz .end_add
+
     mov r11, rax
 
     mov rax, [r8]
     test rax, rax
-    jne .append_node
+    jnz .add_to_end
 
+    ; Lista vacía
     mov [r8], r11
-    mov [r8 + 8], r11
+    mov [r8+8], r11
     jmp .end_add
 
-.append_node:
-    mov rcx, [r8 + 8]
+.add_to_end:
+    mov rcx, [r8+8]
     mov [rcx], r11
-    mov [r11 + 8], rcx
-    mov [r8 + 8], r11
+    mov [r11+8], rcx
+    mov [r8+8], r11
 
 .end_add:
     ret
 
 string_proc_list_concat_asm:
-    test rdi, rdi
-    je .fail_concat
+    push rbx
+    push r12
+    push r13
+    push r14
+    push r15
+    sub rsp, 8
 
-    mov r8, rdi        ; list
-    movzx r9d, sil     ; type
-    mov r10, rdx       ; hash extra
+    mov rbx, rdi     ; list
+    movzx r12d, sil  ; type
+    mov r13, rdx     ; hash
+
+    test rbx, rbx
+    jz .ret_null
 
     mov rdi, 1
     call malloc
     test rax, rax
-    je .fail_concat
+    jz .ret_null
+
     mov byte [rax], 0
-    mov r11, rax
+    mov r14, rax         ; new_hash
+    mov r15, [rbx]       ; current_node = list->first
 
-    mov r12, [r8]      ; nodo actual
+.loop:
+    test r15, r15
+    jz .after_loop
 
-.loop_nodes:
-    test r12, r12
-    je .join_extra
+    mov al, [r15+16]
+    cmp al, r12b
+    jne .next_node
 
-    mov al, [r12 + 16]
-    cmp al, r9b
-    jne .next_iter
-
-    mov rdi, r11
-    mov rsi, [r12 + 24]
+    mov rdi, r14
+    mov rsi, [r15+24]
     call str_concat
     test rax, rax
-    je .fail_concat
-    mov rdi, r11
-    mov r11, rax
+    jz .free_and_null
+
+    mov rdi, r14
+    mov r14, rax
     call free
 
-.next_iter:
-    mov r12, [r12]
-    jmp .loop_nodes
+.next_node:
+    mov r15, [r15]
+    jmp .loop
 
-.join_extra:
-    test r10, r10
-    je .done_concat
+.after_loop:
+    test r13, r13
+    jz .ret_r14
 
-    mov rdi, r10
-    mov rsi, r11
+    mov rdi, r13
+    mov rsi, r14
     call str_concat
     test rax, rax
-    je .fail_concat
-    mov rdi, r11
-    mov r11, rax
+    jz .free_and_null
+
+    mov rdi, r14
+    mov r14, rax
     call free
 
-.done_concat:
-    mov rax, r11
+.ret_r14:
+    mov rax, r14
+    add rsp, 8
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbx
     ret
 
-.fail_concat:
+.free_and_null:
+    test r14, r14
+    jz .ret_null
+    mov rdi, r14
+    call free
+
+.ret_null:
     xor rax, rax
+    add rsp, 8
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbx
     ret
