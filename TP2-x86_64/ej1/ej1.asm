b31402b4dadefb03678efbfced143057bc98d48f
Julia Pantin <69682517+Juan-2001@users.noreply.github.com>
2025-04-18 16:59:54 -0300

    Update ej1.asm

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -1,6 +1,12 @@
 %define NULL 0
 %define TRUE 1
 %define FALSE 0
+%define STRUCT_LIST_SIZE 16
+%define STRUCT_NODE_SIZE 32
+%define OFFSET_NEXT 0
+%define OFFSET_PREV 8
+%define OFFSET_TYPE 16
+%define OFFSET_HASH 24
 
 section .data
 
@@ -19,12 +25,12 @@ extern str_concat
 ; Crear lista vacía
 ;==================================
 string_proc_list_create_asm:
-    mov rdi, 16
+    mov rdi, STRUCT_LIST_SIZE
     call malloc
-    test rax, rax
+    cmp rax, 0
     je .fail
-    mov qword [rax], NULL
-    mov qword [rax + 8], NULL
+    mov qword [rax + OFFSET_NEXT], NULL
+    mov qword [rax + OFFSET_PREV], NULL
     ret
 .fail:
     xor rax, rax
@@ -39,21 +45,21 @@ string_proc_node_create_asm:
     push rbx
     push r12
 
-    test rsi, rsi
+    cmp rsi, 0
     je .node_fail
 
     mov r12, rsi
     mov bl, dil
 
-    mov rdi, 32
+    mov rdi, STRUCT_NODE_SIZE
     call malloc
-    test rax, rax
+    cmp rax, 0
     je .node_fail
 
-    mov qword [rax], NULL
-    mov qword [rax + 8], NULL
-    mov byte  [rax + 16], bl
-    mov qword [rax + 24], r12
+    mov qword [rax + OFFSET_NEXT], NULL
+    mov qword [rax + OFFSET_PREV], NULL
+    mov byte  [rax + OFFSET_TYPE], bl
+    mov qword [rax + OFFSET_HASH], r12
 
     pop r12
     pop rbx
@@ -77,20 +83,20 @@ string_proc_list_add_node_asm:
     push r13
     push r14
 
-    mov rbx, rdi      
-    mov r13, rsi  
+    mov rbx, rdi       
+    mov r13, rsi        
     mov r14, rdx       
 
     movzx edi, r13b
     mov rsi, r14
     call string_proc_node_create_asm
-    test rax, rax
+    cmp rax, 0
     je .end
 
-    mov rcx, rax     
+    mov rcx, rax       
 
     mov rax, [rbx]
-    test rax, rax
+    cmp rax, 0
     jne .not_empty
 
     mov [rbx], rcx
@@ -99,8 +105,8 @@ string_proc_list_add_node_asm:
 
 .not_empty:
     mov rdx, [rbx + 8]  
-    mov [rdx], rcx    
-    mov [rcx + 8], rdx  
+    mov [rdx], rcx     
+    mov [rcx + 8], rdx 
     mov [rbx + 8], rcx  
 
 .end:
@@ -123,21 +129,21 @@ string_proc_list_concat_asm:
     push r14
     push r15
 
-    mov rbx, rdi        
-    movzx r12d, sil    
-    mov r13, rdx        
+    mov rbx, rdi       
+    movzx r12d, sil     
+    mov r13, rdx      
 
     mov rdi, 1
     call malloc
-    test rax, rax
+    cmp rax, 0
     je .null
-    mov byte [rax], 0
+    mov byte [rax + OFFSET_NEXT], 0
     mov r14, rax        
 
     mov r15, [rbx]     
 
 .loop:
-    test r15, r15
+    cmp r15, 0
     je .after
 
     mov al, byte [r15 + 16]
@@ -159,13 +165,13 @@ string_proc_list_concat_asm:
     jmp .loop
 
 .after:
-    test r13, r13
+    cmp r13, 0
     je .add
 
     mov rdi, r13
     mov rsi, r14
     call str_concat
-    test rax, rax
+    cmp rax, 0
     je .fail
 
     mov rdi, r14
